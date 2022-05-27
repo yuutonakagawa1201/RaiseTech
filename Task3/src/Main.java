e92449cdbf720c689273b3416a568b9fba2bc2ca
yuutonakagawa1201 <105264573+yuutonakagawa1201@users.noreply.github.com>
2022-05-27 23:59:33 +0900

    修正１回目

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,28 +1,43 @@
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String > userMap = new HashMap<>();
-        userMap.put("クボタ","創業者：久保田権四郎 設立：１８９０年");
-        userMap.put("ヤンマー","創業者：山岡孫吉 設立：１９１２年");
-        userMap.put("イセキ","創業者：井関邦三郎 設立：１９２６年");
+        Map<String, String> founding = new HashMap<>();
+        founding.put("クボタ", "創業者：久保田権四郎 設立：１８９０年");
+        founding.put("ヤンマー", "創業者：山岡孫吉 設立：１９１２年");
+        founding.put("イセキ", "創業者：井関邦三郎 設立：１９２６年");
 
-        System.out.println(userMap.get("クボタ"));
-        System.out.println(userMap.get("ヤンマー"));
-        System.out.println(userMap.get("イセキ"));
+//        System.out.println(founding.get("クボタ"));
+//        System.out.println(founding.get("ヤンマー"));
+//        System.out.println(founding.get("イセキ"));
+
+        for(Map.Entry<String, String> entry : founding.entrySet()){
+            System.out.println(entry.getKey() + entry.getValue());
+        }
+
+
+        founding.forEach((k , v) -> {
+            System.out.println(k + v);
+        });
+
 
         String [] name = {"共立", "丸山", "昭信"};
 
-        int string = 0;
+        int i = 3;
         try{
-            System.out.println(name[string] + "は防除機メーカーです。");
+            System.out.println(name[i] + "は防除機メーカーです。");
         }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println(string + "番目は農業機械メーカーではありません。");
+            System.out.println(i + "番目は農業機械メーカーではありません。");
         } finally {
             System.out.println("最終処理です");
         }
+
+        List<String> maker = new ArrayList<>();
+        maker.add("共立");
+        maker.add("丸山");
+        maker.add("昭信");
+
     }
 }
 
