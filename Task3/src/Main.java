31105ecd756313352f11a77ac431ba808d4334e5
yuutonakagawa1201 <105264573+yuutonakagawa1201@users.noreply.github.com>
2022-05-30 14:31:47 +0900

    {}追加、Map変数名変更

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -3,18 +3,18 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String> company = new HashMap<>();
-        company.put("クボタ", "創業者：久保田権四郎 設立：１８９０年");
-        company.put("ヤンマー", "創業者：山岡孫吉 設立：１９１２年");
-        company.put("イセキ", "創業者：井関邦三郎 設立：１９２６年");
+        Map<String, String> companyHistoryByName = new HashMap<>();
+        companyHistoryByName.put("クボタ", "創業者：久保田権四郎 設立：１８９０年");
+        companyHistoryByName.put("ヤンマー", "創業者：山岡孫吉 設立：１９１２年");
+        companyHistoryByName.put("イセキ", "創業者：井関邦三郎 設立：１９２６年");
 
 
-        for(Map.Entry<String, String> entry : company.entrySet()){
+        for(Map.Entry<String, String> entry : companyHistoryByName.entrySet()){
             System.out.println(entry.getKey() + entry.getValue());
         }
 
 
-        company.forEach((k , v) -> {
+        companyHistoryByName.forEach((k , v) -> {
             System.out.println(k + v);
         });
 
@@ -30,13 +30,14 @@ public class Main {
             System.out.println("最終処理です");
         }
 
-        List<String> maker = new ArrayList<>();
-        maker.add("共立");
-        maker.add("丸山");
-        maker.add("昭信");
+        List<String> makers = new ArrayList<>();
+        makers.add("共立");
+        makers.add("丸山");
+        makers.add("昭信");
 
-        for(i =0; i <maker.size(); i++)
-        System.out.println("防除機メーカーは" + maker.get(i) + "です。");
+        for(i =0; i <makers.size(); i++) {
+            System.out.println("防除機メーカーは" + makers.get(i) + "です。");
+        }
     }
 }
 
