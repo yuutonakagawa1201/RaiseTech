304f34ce0f62f930d595d74b3b6a188694dcc96d
yuutonakagawa1201 <105264573+yuutonakagawa1201@users.noreply.github.com>
2022-06-03 14:26:01 +0900

    第４回課題

---
+++ Task4/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = List.of("kyouritu", "maruyama", "syoushin", "kubota", "yannma", "iseki",
+                "mitubisi", "takakita", "konnma", "isii", "satake", "yamamoto", "suta-", "nipuro", "kobasi");
+//        Ｍが含まれる名前は？
+        List<String> result = names.stream().filter(name -> name.contains("m")).toList();
+        System.out.println(result);
+
+//        Mから始まる名前は？
+        long count = names.stream().filter(name -> name.startsWith("m")).count();
+        System.out.println(count);
+
+//        kyourituと含まれる名前はあるのか？　true か falseで返す
+        boolean hasKyouritu = names.stream().anyMatch(name -> name.equals("kyouritu"));
+        System.out.println(hasKyouritu);
+
+//        nameを大文字にして出力する
+//        names.stream().map(name -> name.toUpperCase(Locale.ROOT)).forEach(name -> System.out.println(name));
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
+    }
+}
